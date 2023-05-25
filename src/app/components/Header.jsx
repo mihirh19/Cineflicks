a7741f6e90e889d508f316ecb9e50213c9465398
mihirh19 <miheerhadvani990@gmail.com>
2023-05-25 16:21:49 +0530

    logos added

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,16 @@
+import styles from '@/app/styles/navbar.module.css';
+
+import React from 'react'
+
+const Header = () => {
+   return (
+      <header className={styles.main_header}>
+         <div className={styles.navbar_brand}>
+
+         </div>
+
+      </header>
+   )
+}
+
+export default Header
